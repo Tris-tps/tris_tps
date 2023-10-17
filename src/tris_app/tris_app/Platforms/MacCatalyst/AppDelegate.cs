7087d2a3cde2caefe6a52a1ff4f2f1a09aa85376
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-10-17 17:53:17 +0200

    create app on main

---
+++ src/tris_app/tris_app/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace tris_app
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
