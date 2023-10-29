d7dcc49331ae80e51698e31b8f985415a2ef5f90
AndreaPanzeri <andrea.panzeri.2005@issgreppi.it>
2023-10-29 18:14:39 +0100

    aggiornamento titoli
    Co-authored-by: GabrieleViga <gabriele.vigano@issgreppi.it>

--- src/ConsoleApp1/ProvaPanz/Program.cs
+++ src/ConsoleApp1/ProvaPanz/Program.cs
@@ -56,6 +56,7 @@ namespace ProvaPanz
             string vittoria = ResultsPage.Vincita();
             string sconfitta = ResultsPage.Perdita();
             string pareggio = ResultsPage.Pareggio();
+
             Console.WriteLine(vittoria);
             Console.WriteLine(sconfitta);
             Console.WriteLine(pareggio);
