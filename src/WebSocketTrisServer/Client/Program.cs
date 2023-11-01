4239bfe7dacfa44caa2e44355cd60dfdf55f50bb
Gabriele Viganò <gabriele.vigano@issgreppi.it>
2023-11-01 17:02:40 +0100

    Inizio implementazione "gioca di nuovo"
    Co-Authored-By: Marco Burstein <marco@marco.how>

--- src/WebSocketTrisServer/Client/Program.cs
+++ src/WebSocketTrisServer/Client/Program.cs
@@ -185,18 +185,21 @@ public class Program
             Console.Clear();
             ResultsPage.DisplayWin();
             isGameFinished = !isGameFinished;
+            DispalyChoose();
         }
         else if (data == "Hai Perso!")
         { 
             Console.Clear();
             ResultsPage.DisplayLose();
             isGameFinished = !isGameFinished;
+            DispalyChoose();
         }
         else if (data == "La partita è finita in pareggio")
         {
             Console.Clear();
             ResultsPage.DisplayDraw();
             isGameFinished = !isGameFinished;
+            DispalyChoose();
         }
     }
 
@@ -260,5 +263,71 @@ public class Program
            
     }
     */
+    private static void DispalyChoose()
+    {
+        Console.SetCursorPosition(0, 16);
+        Console.WriteLine("Vuoi giocare ancora?");
 
+        string frecciaSelezioneDx = "────>";
+        string frecciaSelezioneSx = "<────";
+
+        Console.SetCursorPosition(31, 18);
+        Console.WriteLine("Gioca ancora", Color.Red);
+
+        Console.SetCursorPosition(31, 20);
+        Console.Write("ESCI");
+
+        Console.SetCursorPosition(24, 18);
+        Console.Write(frecciaSelezioneDx);
+
+        int mode = 0;
+        int cont = 0;
+
+        ConsoleKey key;
+        do
+        {
+            key = Console.ReadKey(true).Key;
+
+            if (key == ConsoleKey.UpArrow)
+            {
+                cont = 0;
+            }
+            else if (key == ConsoleKey.DownArrow)
+            {
+                cont = 1;
+            }
+
+            if (cont == 0)
+            {
+                Console.SetCursorPosition(24, 20);
+                Console.Write("      ");
+                Console.SetCursorPosition(24, 18);
+                Console.Write(frecciaSelezioneDx);
+                Console.SetCursorPosition(31, 20);
+                Console.WriteLine("ESCI", Color.White);
+                Console.SetCursorPosition(31, 18);
+                Console.WriteLine("Gioca ancora", Color.Red);
+                mode = 1;
+            }
+            else
+            {
+                Console.SetCursorPosition(24, 18);
+                Console.Write("      ");
+                Console.SetCursorPosition(24, 20);
+                Console.Write(frecciaSelezioneDx);
+                Console.SetCursorPosition(31, 18);
+                Console.Write("Gioca ancora", Color.White);
+                Console.SetCursorPosition(31, 20);
+                Console.WriteLine("ESCI", Color.Red);
+                mode = 2;
+            }
+        } while (key != ConsoleKey.Enter);
+        if(mode == 1)
+        {
+            HomePage.Gioca();
+            client.Send("b");
+            GamePage.DisplayTable();
+            
+        }
+    }
 }
